5efb38f508b707063aa6c11d11a4ad2f279b2297
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-23 12:31:35 -0700

    successfully merged mvp

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -18,7 +18,7 @@ end
 post '/results' do
   if params[:user_input] == session[:text]
     session[:endtime] = Time.now
-    session[:wpm] = 8/(session[:endtime]-session[:starttime]) 
+    session[:wpm] = 8*60/(session[:endtime]-session[:starttime]) 
     redirect '/results'
   #else
   #   "didn't type correctly"
